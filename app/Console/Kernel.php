303e97c6dfebff94efcfc056dbd283e4ac84b8b4
Naumov Konstantin <naumov-web@yandex.ru>
2020-06-26 18:50:38 +0300

    Init application

---
+++ app/Console/Kernel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console;
+
+use App\Console\Commands\KeyGenerateCommand;
+use Illuminate\Console\Scheduling\Schedule;
+use Laravel\Lumen\Console\Kernel as ConsoleKernel;
+
+/**
+ * Class Kernel
+ * @package App\Console
+ */
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        KeyGenerateCommand::class
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param Schedule $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        //
+    }
+}
